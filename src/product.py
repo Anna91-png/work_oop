85b2d07120f21f1420b95f0741273d71598b4209
Anna <mirzeyevaaygun34@gmail.com>
2025-07-19 15:59:02 +0400

    были поправлены тесты которые не прошли проверку

--- src/product.py
+++ src/product.py
@@ -1,21 +1,28 @@
 from src.BaseProduct import BaseProduct
 from src.InfoMixin import InfoMixin
 
+
 class Product(InfoMixin, BaseProduct):
     product_count = 0
 
     def __init__(self, name, description, price, quantity):
-        super().__init__(name, description, price, quantity)
+        self._price = None
+        self.name = name
+        self.description = description
+        self.price = price  # работает через сеттер
+        self.quantity = quantity
         Product.product_count += 1
 
-    @classmethod
-    def new_product(cls, prod_dict):
-        return cls(
-            prod_dict["name"],
-            prod_dict["description"],
-            prod_dict["price"],
-            prod_dict["quantity"]
-        )
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self._price = value
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
     def get_info(self) -> str:
         return f"{self.name} - {self.description} ({self.price} руб., {self.quantity} шт.)"
@@ -27,6 +34,15 @@ class Product(InfoMixin, BaseProduct):
         return f"Product('{self.name}', '{self.description}', {self.price}, {self.quantity})"
 
     def __add__(self, other):
-        if type(self) is not type(other):
-            raise TypeError("Cannot add products of different types")
-        return self.price + other.price
+        if not isinstance(other, Product):
+            raise TypeError("Можно складывать только продукты одного типа")
+        return self.price * self.quantity + other.price * other.quantity
+
+    @classmethod
+    def new_product(cls, prod_dict):
+        return cls(
+            prod_dict["name"],
+            prod_dict["description"],
+            prod_dict["price"],
+            prod_dict["quantity"]
+        )
